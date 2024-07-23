adf5fc7da606d47fd872ef3e8b2c092adb039ea4
Mostafa Hussien <mostafahass314@gmail.com>
2024-07-23 19:20:09 +0300

    fix : removed changes for swagger

--- SenderEmail/src/main/java/com/example/SenderEmail/SenderEmailApplication.java
+++ SenderEmail/src/main/java/com/example/SenderEmail/SenderEmailApplication.java
@@ -1,9 +1,26 @@
 package com.example.SenderEmail;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Demo Email Sender",
+                description = "Very basic Demo",
+                contact = @Contact(
+                        email = "mostafadfrg@gmail.com",
+                        name = "Mostafa Hussein"
+                ),
+                version = "0.0.1"
+        ),
+        servers = {
+                @Server(url = "http://localhost:8080", description = "Development Server")
+        }
+)
 public class SenderEmailApplication {
 
     public static void main(String[] args) {
