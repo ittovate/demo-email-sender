df2e241bd7552db906977e4d23bbdb302a9951ba
Mostafa Hussien <mostafahass314@gmail.com>
2024-07-23 19:25:17 +0300

    fix : changes for PR

--- SenderEmail/src/main/java/com/example/SenderEmail/exception/GlobalExceptionHandler.java
+++ SenderEmail/src/main/java/com/example/SenderEmail/exception/GlobalExceptionHandler.java
@@ -1,11 +1,29 @@
 package com.example.SenderEmail.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.server.ResponseStatusException;
 
 @ControllerAdvice
-public class ExceptionHandler {
+public class GlobalExceptionHandler {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleResponseStatusException
+ private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class) ;
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<ErrorResponse> handleResponseStatusException(ResponseStatusException ex) {
+        int statusCode = ex.getStatusCode().value();
+        String message = ex.getReason();
+        logger.error("handle Response Status Exception: {} " ,message  );
+        ErrorResponse errorResponse = new ErrorResponse(statusCode, message );
+        return new ResponseEntity<>(errorResponse, ex.getStatusCode());
+    }
+
+
+
+
 }
