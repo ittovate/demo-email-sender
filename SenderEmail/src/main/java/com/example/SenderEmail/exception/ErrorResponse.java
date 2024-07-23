0ba857dfc6f7e5ef043f518f2745bdd685d4cbd9
Mostafa Hussien <mostafahass314@gmail.com>
2024-07-23 19:30:02 +0300

    feat : add remaining logic for Exception handler

--- SenderEmail/src/main/java/com/example/SenderEmail/exception/ErrorResponse.java
+++ SenderEmail/src/main/java/com/example/SenderEmail/exception/ErrorResponse.java
@@ -1,4 +1,24 @@
 package com.example.SenderEmail.exception;
 
 public class ErrorResponse {
+    private int statusCode ;
+    private  String  message ;
+
+
+    public ErrorResponse(int statusCode, String message) {
+        this.statusCode = statusCode;
+        this.message = message;
+
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+
+
 }
