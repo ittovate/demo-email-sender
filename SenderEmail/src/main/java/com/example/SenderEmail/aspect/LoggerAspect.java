0370b34cf5d531f3e437ae065bcc9d2285bd1ab8
MostafaHussien <mostafahass314@gmail.com>
2024-07-28 22:49:39 +0300

    Added: JavaDocs for your classes and methods

--- SenderEmail/src/main/java/com/example/SenderEmail/aspect/LoggerAspect.java
+++ SenderEmail/src/main/java/com/example/SenderEmail/aspect/LoggerAspect.java
@@ -2,17 +2,39 @@ package com.example.SenderEmail.aspect;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+/**
+ * This class is an Aspect component used for logging method executions and exceptions in the application.
+ * It uses Aspect-Oriented Programming (AOP) .
+ * <p>
+ * The {@code LoggerAspect} class contains two main aspects:
+ * <ul>
+ *    1-  <li>{@code logAfterThrowing}: Logs information about exceptions thrown by methods in the
+ *     {@code com.example.SenderEmail.service} package.</li>
+ *     <br>
+ *    2- <li>{@code loggerAround}: Logs entry, exit, and execution time for the
+ *     {@code sendEmail} method in the {@code EmailService} class.</li>
+ * </ul>
+ */
 @Aspect
 @Component
 public class LoggerAspect {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggerAspect.class);
 
+
+    /**
+     * Logs information about exceptions thrown by methods in the
+     * {@code com.example.SenderEmail.service} package.
+     *
+
+     */
     @AfterThrowing( value = "execution(* com.example.SenderEmail.service.*.*(..))", throwing = "ex")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable ex) {
         String  methodName = joinPoint.getSignature().getName() ;
@@ -21,6 +43,11 @@ public class LoggerAspect {
     }
 
 
+    /**
+     * Logs entry, exit, and execution time for the {@code sendEmail} method in the
+     * {@code EmailService} class.
+
+     */
     @Around("execution( * com.example.SenderEmail.service.EmailService.sendEmail(..) )")
     public void loggerAround (ProceedingJoinPoint joinPoint) throws Throwable {
 
