944e5325deab91d5998390cb2cadfe45561c9d86
mustafa-abuelmagd <mustafa-abuelmagd@gmail.com>
2024-08-14 17:12:27 +0300

    fix: remove unnecessary dependency

--- SenderEmail/src/main/java/com/example/senderemail/SenderEmailApplication.java
+++ SenderEmail/src/main/java/com/example/senderemail/SenderEmailApplication.java
@@ -1,8 +1,6 @@
 package com.example.senderemail;
 
 
-import io.github.cdimascio.dotenv.Dotenv;
-import io.github.cdimascio.dotenv.DotenvEntry;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
@@ -11,9 +9,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
 
@@ -40,16 +35,6 @@ public class SenderEmailApplication {
      * @param args
      */
     public static void main(String[] args) {
-
-
-        Set<Map.Entry<String, String>> envVars = System.getenv().entrySet();
-
-        for (int i = 0; i < envVars.size(); i++) {
-            String key = envVars.toArray()[i].toString().split("=")[0];
-            String value = envVars.toArray()[i].toString().split("=")[1];
-            System.out.println("Key:::: " + key + "    Value::: " + value);
-        }
-        System.out.println("\n\n\n\n\n\n\n\n\n");
 
         SpringApplication.run(SenderEmailApplication.class, args);
     }
