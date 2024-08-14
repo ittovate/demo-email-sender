10a7ceaf4f56fbb26baa0a06c539809534179528
mustafa-abuelmagd <mustafa-abuelmagd@gmail.com>
2024-08-14 13:17:38 +0300

    feat: initial dockerfile

--- SenderEmail/src/main/java/com/example/senderemail/SenderEmailApplication.java
+++ SenderEmail/src/main/java/com/example/senderemail/SenderEmailApplication.java
@@ -36,54 +36,20 @@ import java.util.Set;
 @EnableAsync
 public class SenderEmailApplication {
 
-    public static void loadDotenv(String filePath) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                line = line.trim();
-                if (!line.isEmpty() && !line.startsWith("#")) {
-                    String[] parts = line.split("=", 2);
-                    if (parts.length == 2) {
-                        String key = parts[0].trim();
-                        String value = parts[1].trim();
-                        System.out.println("key:::  " + key + "    value:: " + value);
-                        System.setProperty(key, value);
-                    }
-                }
-            }
-        }
-    }
-
-
     /**
      * @param args
      */
     public static void main(String[] args) {
-//        try {
-//            // Adjust the path to your .env file
-//            loadDotenv(args[0] + ".env");
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+
 
         Set<Map.Entry<String, String>> envVars = System.getenv().entrySet();
-        System.out.println("env vars :: :: :" + envVars);
 
-//        String[] keys = (String[]) envVars.keySet().toArray();
         for (int i = 0; i < envVars.size(); i++) {
             String key = envVars.toArray()[i].toString().split("=")[0];
             String value = envVars.toArray()[i].toString().split("=")[1];
             System.out.println("Key:::: " + key + "    Value::: " + value);
         }
-
-        // Add Environment Variables to the application context before start
-//        Dotenv dotenv = Dotenv.load();
-//        for (DotenvEntry entry : dotenv.entries()) {
-//            System.out.println("KEY :::: " + entry.getKey() + "   VALUE:::: " + entry.getValue());
-//            System.setProperty(entry.getKey(), entry.getValue());
-//        }
-
-        // Start the application
+        System.out.println("\n\n\n\n\n\n\n\n\n");
 
         SpringApplication.run(SenderEmailApplication.class, args);
     }
